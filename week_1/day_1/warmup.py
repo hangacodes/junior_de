5e54ad4153fe60a9f792875bee73c4f92d284c3b
Cristian Hanga <hanga.codes@gmail.com>
2026-02-16 16:00:28 +0300

    W1D1: warmup and story, first scripts

---
+++ week_1/day_1/warmup.py
@@ -0,0 +1,16 @@
+print("Printing some lines to warmup")
+print("This is fun, can I write anything?")
+print("Hello, data engineering world. I already feel like a pro") #joking lol
+
+#This script will tell you my age
+
+print("I am 30 years old")
+
+#fix_quotes
+#broken script : print("I will learn data engineering!)
+#fixed:
+print("I will learn data engineering!")
+
+print("=== START ===")
+print("My script is running")
+print("=== END ===")
