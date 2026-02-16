9eea37e472882b6350f73995c1a798b742488eef
Cristian Hanga <hanga.codes@gmail.com>
2026-02-16 15:30:49 +0300

    complete day 1 exercises and reorganize folders

--- week1/setup/hello.py
+++
@@ -1,5 +0,0 @@
-print("Hello, data engineering!")
-
-print("How does this work?")
-
-print("Trying to see what commit actually does")
---
+++ week_1/day_1/exercises.py
@@ -0,0 +1,27 @@
+print("Hello world! Hello data engineering!")
+print("This is day 1 of learning how to code")
+print("This is a tiny program")
+print("I am inside a .py file, wow")
+print("This is a string literal, literally what I'm writing between quotes right now, is the string literal")
+
+#This is a comment. Python ignores it - this help humans understand what the code does, what to watch out for.
+
+#Example of syntax error: must be kept as a comment, so we can still run the file. 
+#print("This string is missing the closin quote) 
+
+print("This is the corrected version that can run") 
+
+#runtime error example
+print("This line runs.")
+#If you uncomment the next line, Python will crash while running ( runtime error ). 
+#print(Hello)
+
+print("Keeping the broken line commented let's the script run")
+
+#Example 1:
+print("Hello, data engineering world!")
+
+#Example 2: Multiple lines run top to bottom
+print("Line 1")
+print("Line 2")
+print("Line 3")
