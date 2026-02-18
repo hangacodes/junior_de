5d9b89db8780406bdc06ccf4389a79975087bbce
Cristian Hanga <hanga.codes@gmail.com>
2026-02-18 18:24:20 +0300

    W1D2: Complete semi-guided drills

---
+++ week_1/day_2/w1d2_semiguided.py
@@ -0,0 +1,52 @@
+#Problem 1: Complete the “profile” variables
+#TODO: Fill in values 
+first = "Cristian"
+last = "Hanga"
+age_text = "30"
+
+#TODO: Convert age_text into an int called age
+
+#TODO print first, last, age and the type of age
+
+age = int(age_text)
+
+print(first)
+print(last)
+print(age)
+
+print(type(age))
+print("\n")
+#Problem 2: Turn text into numbers (two conversions)
+
+raw_count = "120"
+raw_ratio = "0.75"
+
+#TODO: Convert raw_count into an int called count
+#TODO: Convert raw_ratio into a float called ratio
+
+#TODO: Print the types of raw_count, count, raw_ratio, ratio
+
+count = (int(raw_count))
+ratio = (float(raw_ratio))
+
+print(type(raw_count))
+print(type(count))
+print(type(raw_ratio))
+print(type(ratio))
+print("\n")
+
+#Problem 3: Make a “settings summary”
+project = "mini_etl"
+retries_text = "3"
+
+#TODO: Convert retries_text into an int called retries
+
+#TODO: Print project, retries, and their types
+
+retries = int(retries_text)
+
+print(project)
+print(retries)
+
+print(type(project))
+print(type(retries))
