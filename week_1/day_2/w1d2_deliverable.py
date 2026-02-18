9a20b9982ae9c267455c68b7d65e87c485be5503
Cristian Hanga <hanga.codes@gmail.com>
2026-02-18 18:34:28 +0300

    Delivered deliverable

---
+++ week_1/day_2/w1d2_deliverable.py
@@ -0,0 +1,25 @@
+print("PIPELINE CONFIG CARD")
+print("--------------------")
+project_name = "Zero to Hired Data Engineer"
+source_system = "junior_de_database"
+first_name = "Cristian"
+day = "2"
+price = 20
+discounted_price = 19.99
+is_discounted = True
+
+print(project_name)
+print(source_system)
+print(first_name)
+print(int(day))
+print(price)
+print(discounted_price)
+print(is_discounted)
+print("\n")
+
+print("TYPES CHECK")
+print("-----------")
+print(type(price))
+print(type(first_name))
+print(type(day))
+print(type(is_discounted))
