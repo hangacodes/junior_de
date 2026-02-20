530600d7f071b61365af5e6b32e16afe48dae589
Cristian Hanga <hanga.codes@gmail.com>
2026-02-20 14:40:01 +0300

    Activation drills done

---
+++ week_1/day_4/drills.py
@@ -0,0 +1,22 @@
+
+'''Activation drills:
+1. Given `s = "DATA"`, print the first character.
+2. Given `s = "DATA"`, print the last character using a negative index.
+3. Given `s = "engineer"`, print `len(s)`.
+4. Given `s = "abcdef"`, print `s[1:4]`.
+5. Given `s = "abcdef"`, print `s[::-1]`.'''
+
+s = "DATA"
+
+print(s[0])
+print(s[-1])
+
+s1 = "engineer"
+
+print(len(s1))
+
+s2 = "abcdef"
+
+print(s2[1:4])
+print(s2[::-1])
+
